4cd9f761723a2f13a18d3a8b7f67582df8f5bd49
Thomas Häny <dev@haeny.de>
2019-06-12 18:49:40 +0200

    Gets the ToggleBox in fmh-toolbox/fmh-utils working to open/close the Zinsen-Tabelle

--- fmh-toolbox/js/fmh-utils.js
+++ fmh-toolbox/js/fmh-utils.js
@@ -1,37 +1,46 @@
+/**********************************************************************************************************************
+ * ToggleBox: a little helper to show/hide a defined box by click on a button
+ * Example:
+    <div class="toggleBox__wrapper">
+      <div class="toggleBox__button">
+        The content of the button goes here
+      </div>
+      <div class="toggleBox__box">
+        The content of the box goes here ...
+      </div>
+    </div>
+ * The box and button must be somewhere below the wrapper, but not necessarily direct below
+ *
+ * The box gets the 'showBox'-class toggled
+ * and the button the 'isOpen'-class
+ *
+ * The styles are defined in fmh-utils.scss/css
+*/
+
 /**
-const fmh = fmh || {};
-
-// same functions as above
-fmh.util = (function() {
-    function toggleBox(buttonClass, boxClass) {
-      const boxToggleClass = 'showBox';
-      const buttonToggleClass = 'isOpen';
-
-      let button = document.querySelector(buttonClass);
-      let box = document.querySelector(boxClass);
-
-      button.addEventListener('click', function() {
-        box.classList.toggle(boxToggleClass);
-        button.classList.toggle(buttonToggleClass);
-      });
-    }
-
-    return {
-        toggleBox: toggleBox
-    };
-})();
-
-fmh.util.toggleBox('.fmh-util__toggleBox--button', '.fmh-util__toggleBox--box');
-**/
-
-function toggleBox(buttonClass, boxClass) {
-  const boxToggleClass = 'showBox';
-  const buttonToggleClass = 'isOpen';
-
-  let button = document.querySelector('.'+buttonClass);
-  let box = document.querySelector('.'+boxClass);
-
-  box.classList.toggle(boxToggleClass);
-  button.classList.toggle(buttonToggleClass);
-
+ * toggleBox: open or close the box and change the button-class
+ * @param event
+ * @param button
+ * @param box
+ */
+function toggleBox(event, button, box) {
+  button.classList.toggle('isOpen');
+  box.classList.toggle('showBox')
 }
+
+/**
+ * registerToggleBoxes: register click-events to all toggleBoxes on page
+ */
+function registerToggleBoxes() {
+  let tbWrappers = document.querySelectorAll('.toggleBox__wrapper');
+  tbWrappers.forEach(function(tbWrapper) {
+    let tbButton = tbWrapper.querySelector('.toggleBox__button');
+    let tbBox = tbWrapper.querySelector('.toggleBox__box');
+    tbButton.addEventListener("click", function (e) { toggleBox(e, tbButton, tbBox) }, false );
+  });
+}
+
+window.onload = registerToggleBoxes;
+
+
+
